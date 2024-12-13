5d84a8c84375175c6d9dfb9e54098d9692cad1e6
Kumkum Asati <155238180+kumkumasati@users.noreply.github.com>
2024-12-13 19:39:13 +0530

    Create style.js in root directory

---
+++ style.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const faBars = document.querySelector('.fa-bars');
+    const navbar = document.querySelector('.navbar');
+    const header = document.querySelector('header');
+  
+   
+    faBars.addEventListener('click', () => {
+      faBars.classList.toggle('fa-times');
+      navbar.classList.toggle('nav-toggle');
+    });
+  
+    
+    const handleScrollLoad = () => {
+      faBars.classList.remove('fa-times');
+      navbar.classList.remove('nav-toggle');
+  
+      if (window.scrollY > 30) {
+        header.classList.add('header-active');
+      } else {
+        header.classList.remove('header-active');
+      }
+    };
+  
+    
+    window.addEventListener('scroll', handleScrollLoad);
+    window.addEventListener('load', handleScrollLoad);
+  })
